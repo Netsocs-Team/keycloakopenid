ba2e3bb1b8443cc1af200a18c1fa47fb4d880276
gmd3v <gussmotab@gmail.com>
2025-04-03 15:39:18 -0400

    feat: add InsecureSkipVerify support to the plugin

--- config_parser.go
+++ config_parser.go
@@ -7,11 +7,13 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Config struct {
 	KeycloakURL        string `json:"url"`
+	InsecureSkipVerify bool   `json:"insecure_skip_verify"`
 	ClientID           string `json:"client_id"`
 	ClientSecret       string `json:"client_secret"`
 	KeycloakRealm      string `json:"keycloak_realm"`
@@ -22,6 +24,7 @@ type Config struct {
 	UserHeaderName     string `json:"user_header_name"`
 	IgnorePathPrefixes string `json:"ignore_path_prefixes"`
 
+	InsecureSkipVerifyEnv string `json:"insecure_skip_verify_env"`
 	ClientIDFile          string `json:"client_id_file"`
 	ClientSecretFile      string `json:"client_secret_file"`
 	KeycloakURLEnv        string `json:"url_env"`
@@ -37,6 +40,7 @@ type Config struct {
 type keycloakAuth struct {
 	next               http.Handler
 	KeycloakURL        *url.URL
+	InsecureSkipVerify bool
 	ClientID           string
 	ClientSecret       string
 	KeycloakRealm      string
@@ -112,6 +116,19 @@ func readConfigEnv(config *Config) error {
 		}
 		config.KeycloakURL = strings.TrimSpace(keycloakUrl)
 	}
+
+	if config.InsecureSkipVerifyEnv != "" {
+		insecureSkipVerify := os.Getenv(config.InsecureSkipVerifyEnv)
+		if insecureSkipVerify == "" {
+			return errors.New("InsecureSkipVerifyEnv referenced but NOT set")
+		}
+		insecureSkipVerifyBool, err := strconv.ParseBool(insecureSkipVerify)
+		if err != nil {
+			return err
+		}
+		config.InsecureSkipVerify = insecureSkipVerifyBool
+	}
+
 	if config.ClientIDEnv != "" {
 		clientId := os.Getenv(config.ClientIDEnv)
 		if clientId == "" {
@@ -225,5 +242,6 @@ func New(uctx context.Context, next http.Handler, config *Config, name string) (
 		UserClaimName:      userClaimName,
 		UserHeaderName:     userHeaderName,
 		IgnorePathPrefixes: ignorePathPrefixes,
+		InsecureSkipVerify: config.InsecureSkipVerify,
 	}, nil
 }
